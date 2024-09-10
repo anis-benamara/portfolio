b0398829da1dba00e51aaafbad130642c74ee021
Anis Ben Amara <anis.b.amara@gmail.com>
2024-09-10 23:38:06 +0100

    update fetchUrl

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,4 +5,4 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
-export const fetchUrl = "http://localhost:3000/api"
+export const fetchUrl = process.env.NODE_ENV === 'development' ? "http://localhost:3000/api" : "https://portfolio-anis-ben.vercel.app//api"
