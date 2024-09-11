80ec9eed785dc70364d6b70f4fdc2614f799731b
Anis Ben Amara <anis.b.amara@gmail.com>
2024-09-11 23:49:06 +0100

    useSwr

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,42 +1,58 @@
 import { db } from "@/db";
 
-export function GET() {
-    return new Response('Hello', { status: 200 })
+export async function GET() {
+  try {
+    const data = await db.blog.findMany({
+      take: 10,
+      select: {
+        title: true,
+        category: true,
+        slug: true,
+      },
+      orderBy: [{ view_count: 'desc'}]
+    });
+
+    return Response.json(data)
+  } catch (error) {
+    console.error("Error fetching blog posts", error);
+    throw new Error("Failed to fetch blog posts");
+  }
 }
 
 export async function POST(request: Request) {
-    const { slug, title, category } = await request.json();
+  const { slug, title, category } = await request.json();
 
-    try {
-        const existingPost = await db.blog.findUnique({
-            where: {
-                slug
-            }
-        })
+  try {
+    const existingPost = await db.blog.findUnique({
+      where: {
+        slug,
+      },
+    });
 
-        if (existingPost) {
-            await db.blog.update({
-                where: {
-                    slug
-                },
-                data: {
-                    view_count: {
-                        increment: 1},
-                }
-            })
-        } else {
-            await db.blog.create({
-                data: {
-                    slug,
-                    title,
-                    category,
-                }
-            })
-        }
-    } catch (error) {
-        console.error("Error updading page view", error)
-        return new Response("Failed to post to DB", { status: 500 })
+    if (existingPost) {
+      await db.blog.update({
+        where: {
+          slug,
+        },
+        data: {
+          view_count: {
+            increment: 1,
+          },
+        },
+      });
+    } else {
+      await db.blog.create({
+        data: {
+          slug,
+          title,
+          category,
+        },
+      });
     }
+  } catch (error) {
+    console.error("Error updading page view", error);
+    return new Response("Failed to post to DB", { status: 500 });
+  }
 
-    return new Response("Susccessfully posted to DB", { status: 200 })
+  return new Response("Susccessfully posted to DB", { status: 200 });
 }
--- src/components/home/PopularPosts.tsx
+++ src/components/home/PopularPosts.tsx
@@ -1,17 +1,27 @@
+"use client";
+
+import Link from "next/link";
+import useSWR from "swr";
+
 import { popularPosts } from "@/lib/placeholder-data";
+import { fetcher, fetchUrl } from "@/lib/utils";
+
 import { Icons } from "../Icons";
 
 export default function PopularPosts() {
+  const { data, error, isLoading } = useSWR(fetchUrl, fetcher);
+
+  if (error) return <div>Failed to load</div>;
+  if (isLoading) return <div>Loading....</div>;
   return (
     <ul className="overflow-auto">
-      {popularPosts.map((post) => (
-        <li
-          key={post.title}
-          className="flex items-center group gap-2 cursor-pointer py-2"
-        >
-          <Icons.arrowRight className="h-6 w-6 group-hover:translate-x-1 transition-all" />
-          <span>{post.title}</span>
-        </li>
+      {data?.map((post) => (
+        <Link href={`/blog/${post.category}/${post.slug}`} key={post.title}>
+          <li className="flex items-center group gap-2 cursor-pointer py-2">
+            <Icons.arrowRight className="h-6 w-6 group-hover:translate-x-1 transition-all" />
+            <span>{post.title}</span>
+          </li>
+        </Link>
       ))}
     </ul>
   );
