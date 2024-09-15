05a31dad329c5c808488525a1341408f74c5a7c9
Anis Ben Amara <anis.b.amara@gmail.com>
2024-09-15 22:01:50 +0100

    About, terms-of-services, privacy-policy pages

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -72,7 +72,7 @@ export default function Footer() {
               </li>
               <li>
                 <Link
-                  href="/terms-of-service"
+                  href="/terms-of-services"
                   className="text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-300"
                 >
                   Terms of Service
